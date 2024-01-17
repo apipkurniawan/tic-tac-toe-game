efac1bb1a5c0d295789d0270c8a1342d253e7d40
apipkurniawan <davitaja92@gmail.com>
2024-01-17 17:32:16 +0700

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,19 @@ import Log from './components/Log';
 import GameOver from './components/GameOver';
 import { WINNING_COMBINATIONS } from './winning-combination';
 
-const initialGameBoard = [
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',
+};
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-function deriveActivePlayer(gameTurns) {
-  let currentPlayer = 'X';
-
-  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
-    currentPlayer = 'O';
-  }
-
-  return currentPlayer;
-}
-
-function App() {
-  const [players, setPlayers] = useState({
-    X: 'Player 1',
-    O: 'Player 2',
-  });
-  const [gameTurns, setgameTurns] = useState([]);
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard = [...initialGameBoard.map((array) => [...array])];
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -39,6 +26,10 @@ function App() {
     gameBoard[row][col] = player;
   }
 
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, players) {
   let winner;
 
   for (const combination of WINNING_COMBINATIONS) {
@@ -57,7 +48,26 @@ function App() {
       winner = players[firstSquareSymbol];
     }
   }
+  return winner;
+}
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS);
+  const [gameTurns, setgameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length == 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -96,13 +106,13 @@ function App() {
         <div id='game-container'>
           <ol id='players' className='highlight-player'>
             <Player
-              initialName='Player 1'
+              initialName={PLAYERS.X}
               symbol='X'
               isActive={activePlayer === 'X'}
               onChangeName={handlePlayerNameChange}
             />
             <Player
-              initialName='Player 2'
+              initialName={PLAYERS.O}
               symbol='O'
               isActive={activePlayer === 'O'}
               onChangeName={handlePlayerNameChange}
